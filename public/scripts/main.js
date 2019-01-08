fdbdf76edc19cc659c7d81012602fbd4758083eb
Davi Rox <roxdavirox@gmail.com>
2019-01-08 00:14:54 -0200

    file upload and get fullPath image url

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -85,7 +85,27 @@ function saveMessage(messageText) {
 // Saves a new message containing an image in Firebase.
 // This first saves the image in Firebase storage.
 function saveImageMessage(file) {
-  // TODO 9: Posts a new image as a message.
+    // 1 - Add a chat message placeholder (a loading icon) that will ultimately get updated with the shared image.
+    firebase.database().ref('/messages/').push({
+      name: getUserName(),
+      imageUrl: LOADING_IMAGE_URL,
+      profilePicUrl: getProfilePicUrl()
+    }).then(function(messageRef) {
+      // 2 - Upload the image to Cloud Storage.
+      var filePath = firebase.auth().currentUser.uid + '/' + messageRef.key + '/' + file.name;
+      return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
+        // 3 - Generate a public URL for the image file.
+        return fileSnapshot.ref.getDownloadURL().then((url) => {
+          // 4 - Update the chat message placeholder with the image's URL.
+          return messageRef.update({
+            imageUrl: url,
+            storageUri: fileSnapshot.metadata.fullPath
+          });
+        });
+      });
+    }).catch(function(error) {
+      console.error('There was an error uploading a file to Cloud Storage:', error);
+    });
 }
 
 // Saves the messaging device token to the datastore.
